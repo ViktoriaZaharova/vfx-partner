6ef0622067bf44e2713e195648342733c30be10d
viktoriazaharova <zaharova181114@gmail.com>
2020-06-19 15:30:52 +0300

    edit

--- app/js/common.js
+++ app/js/common.js
@@ -35,3 +35,34 @@ $('.works-slider').slick({
     prevArrow: '<button type="button" class="slick-prev"></button>',
     nextArrow: '<button type="button" class="slick-next"></button>'
 });
+
+var target = $('.benefits__inner');
+var targetPos = target.offset().top;
+var winHeight = $(window).height();
+var scrollToElem = targetPos - winHeight;
+$(window).scroll(function(){
+    var winScrollTop = $(this).scrollTop();
+    if(winScrollTop > scrollToElem){
+        $({ blurRadius: 5 }).animate(
+            { blurRadius: 0 },
+            {
+                duration: 3500,
+                easing: "swing",
+            }
+        );
+        var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(
+            " "
+        );
+        $(".benefits__number").each(function () {
+            var tcount = $(this).data("count");
+            $(this).animateNumber(
+                {
+                    number: tcount,
+                    easing: "easeInQuad",
+                    numberStep: comma_separator_number_step
+                },
+                1000
+            );
+        });
+    }
+});
